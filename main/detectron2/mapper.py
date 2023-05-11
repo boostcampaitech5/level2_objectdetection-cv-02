976b96385975124ad296891b543274dc0603c73e
nstalways <nstalways@gmail.com>
2023-05-11 07:16:24 +0000

    feat: add feature issue template

--- main/detectron2/mapper.py
+++ main/detectron2/mapper.py
@@ -10,9 +10,8 @@ def MyMapper(dataset_dict):
     image = utils.read_image(dataset_dict['file_name'], format='BGR')
     
     transform_list = [
-        T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
-        T.RandomBrightness(0.8, 1.2),
-        T.RandomContrast(0.6, 1.3),
+        T.Resize((1024, 1024)),
+        T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
     ]
     
     image, transforms = T.apply_transform_gens(transform_list, image)
