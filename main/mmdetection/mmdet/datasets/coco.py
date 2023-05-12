1b0f4c2fc99feaabf246886083384281b02e30d2
Chaewon Lee <lcwon9@gmail.com>
2023-05-12 15:40:27 +0900

    feat : Classwise mAP

--- main/mmdetection/mmdet/datasets/coco.py
+++ main/mmdetection/mmdet/datasets/coco.py
@@ -549,7 +549,7 @@ class CocoDataset(CustomDataset):
                         # area range index 0: all area ranges
                         # max dets index -1: typically 100 per image
                         nm = self.coco.loadCats(catId)[0]
-                        precision = precisions[:, :, idx, 0, -1]
+                        precision = precisions[0, :, idx, 0, 0]
                         precision = precision[precision > -1]
                         if precision.size:
                             ap = np.mean(precision)
