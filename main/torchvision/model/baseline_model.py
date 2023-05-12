feec352341e046be8633628e71a5f93eeeda14c4
nstalways <nstalways@gmail.com>
2023-05-12 05:19:47 +0000

    baseline: implement torchvision

---
+++ main/torchvision/model/baseline_model.py
@@ -0,0 +1,18 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+
+def get_fasterrcnn_resnet50_fpn(pretrained:bool=True, progress:bool=True,
+                                pretrained_backbone:bool=True, num_classes:int=11,
+                                trainable_backbone_layers:int=3):
+    
+
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=pretrained,
+                                                                 progress=progress,
+                                                                 pretrained_backbone=pretrained_backbone,
+                                                                 trainable_backbone_layers=trainable_backbone_layers)
+    
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)                                
+
+    return model
