b944ef6208a11f0abd67906d7580a452da36e0f4
Eumgill98 <wjdghcks02@gmail.com>
2023-05-14 17:37:20 +0000

    fix : Augmentation

---
+++ main/mmdetection/augmentation/BaseAugmentation.py
@@ -0,0 +1,49 @@
+def BaseAugmentation(img_norm_cfg):
+    """
+    img_norm_cfg(dict) : img_normalization setting 
+    """
+    total_pipline = {
+        'train' : [      
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
+            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+        ],
+        'val' : [
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1024, 1024),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(type='Normalize', **img_norm_cfg),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img']),
+                ])
+        ],
+        'test' : [
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1024, 1024),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(type='Normalize', **img_norm_cfg),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img']),
+                ])
+        ]
+    }
+
+    return total_pipline
+
